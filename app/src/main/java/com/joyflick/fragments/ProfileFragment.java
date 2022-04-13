6831e631886c0a19a09aecc709e100d408dace99
DhrumilRana1998 <70586989+DhrumilRana1998@users.noreply.github.com>
2022-04-12 23:07:46 -0400

    Done with signup and login page

--- app/src/main/java/com/joyflick/fragments/ProfileFragment.java
+++ app/src/main/java/com/joyflick/fragments/ProfileFragment.java
@@ -1,15 +1,28 @@
 package com.joyflick.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.joyflick.LoginActivity;
 import com.joyflick.R;
+import com.parse.ParseUser;
 
 public class ProfileFragment extends Fragment {
+
+    public static final String TAG = "ProfileFragment";
+    public final static int PICK_PHOTO_CODE = 1046;
+    private Button btnLogout;
+    private Button btnProfilePhoto;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -17,4 +30,21 @@ public class ProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLogout = view.findViewById(R.id.btnLogout);
+        btnProfilePhoto = view.findViewById(R.id.btnProfilePhoto);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent i = new Intent(getContext(), LoginActivity.class);
+                startActivity(i);
+                getActivity().finish();
+            }
+        });
+    }
+
 }
