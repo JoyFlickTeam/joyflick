e030b61a18fe3d566b09f04d4f41eacd8bd51d06
DhrumilRana1998 <70586989+DhrumilRana1998@users.noreply.github.com>
2022-04-18 14:50:52 -0400

    Trying to implement the home feed fragment and the search bar

---
+++ app/src/main/java/com/joyflick/Adapter/GameAdapter.java
@@ -0,0 +1,5 @@
+package com.joyflick.Adapter;
+
+public class GameAdapter {
+
+}
--- app/src/main/java/com/joyflick/fragments/FeedFragment.java
+++ app/src/main/java/com/joyflick/fragments/FeedFragment.java
@@ -1,19 +1,68 @@
 package com.joyflick.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.joyflick.R;
+import com.joyflick.models.Game;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class FeedFragment extends Fragment {
+
+    public static final String MOVIE = "https://api.rawg.io/api/games?key=8abe1aadb6a6459db418c8ac8239aa05";
+    public static final String TAG = "FeedFragment";
+    List<Game> games;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
-}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(MOVIE, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONObject jsonObject = json.jsonObject;
+                Log.d(TAG, "Async onSuccess");
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    games = Game.fromJsonArray(results);
+                    Log.i(TAG, "Games" + games.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit JSON exception", e);
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "Async onFaliure");
+            }
+        });
+    }
+
+
+}// end of the class
+
