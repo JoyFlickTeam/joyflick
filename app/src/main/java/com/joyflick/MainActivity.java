57b52c404d190169864b0a5b4e873dd813c79399
nekozye <61643110+nekozye@users.noreply.github.com>
2022-04-20 10:18:01 -0400

    Revert "Feat game model json parse"

--- app/src/main/java/com/joyflick/MainActivity.java
+++ app/src/main/java/com/joyflick/MainActivity.java
@@ -1,102 +1,14 @@
 package com.joyflick;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.joyflick.backbone.RAWGConnector;
-import com.joyflick.fragments.FeedFragment;
-import com.joyflick.fragments.PostFragment;
-import com.joyflick.fragments.ProfileFragment;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "MainActivity";
-    private final int REQUEST_CODE = 10;
-
-    private BottomNavigationView bottomNavigationView;
-    final FragmentManager fragmentManager = getSupportFragmentManager();
-
-    //RAWGConnector wrapper
-    //use with care
-    private RAWGConnector rawg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Set top bar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.logo1);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setItemIconTintList(null);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
-                Fragment fragment;
-                switch (menuItem.getItemId()) {
-                    case R.id.action_home:
-                        fragment = new FeedFragment();
-                        break;
-                    case R.id.action_post:
-                        fragment = new PostFragment();
-                        break;
-                    case R.id.action_profile:
-                    default:
-                        fragment = new ProfileFragment();
-                        break;
-                }
-                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
-                return true;
-            }
-        });
-        bottomNavigationView.setSelectedItemId(R.id.action_home);
-
-        rawg = new RAWGConnector();
-
-
-        //temporary check for api. remove it afterwards.
-        JSONObject handler;
-        try {
-            handler = rawg.getJson();
-            Log.i("TAG",handler.toString());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        // Inflate menu
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
-        if(item.getItemId() == R.id.search){
-            // Navigate to search activity
-            Intent intent = new Intent(this, SearchActivity.class);
-            startActivityForResult(intent, REQUEST_CODE);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
